1b6fe8e95052a7ba56945750bb86949d7d03340e
Courtney Sims <csims@cloudflare.com>
2024-08-07 11:22:26 -0500

    Updated parent corp

--- stage-bistro/src/App.js
+++ stage-bistro/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 		Aurora's Italian Bistro - home of the world's tastiest meatball
+		A Sister Cat Industries corporation
         </p>
         <a
           className="App-link"
